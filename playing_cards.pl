ba3a78e66245c6ff35f5648e4b1e43212cc0654c
stevemeier <email@steve-meier.de>
2018-02-25 20:03:12 +0100

    Check 2M shuffles for Pocket Aces

--- playing_cards.pl
+++ playing_cards.pl
@@ -7,31 +7,35 @@ my @suites = ("♤ ","♧ ","♡ ","♢ ");
 my @values = (2 .. 9, "T", "Q", "K", "A");
 
 my @stack;
-foreach my $suit (@suites) {
-  foreach my $value (@values) {
-    push(@stack, "$value$suit");
+initialize_stack(\@stack);
+
+foreach (1 .. 2000000) {
+  fisher_yates_shuffle(\@stack);
+
+  if (($stack[0] =~ /A/) && ($stack[1] =~ /A/)) {
+    print "Aces!\n";
   }
-}
-
-fisher_yates_shuffle(\@stack);
-
-if (($stack[0] =~ /A/) && ($stack[1] =~ /A/)) {
-  print "Aces!\n";
-} else {
-  print "No Aces\n";
 }
 
 exit;
 
 # from http://www.perlmonks.org/?node_id=1869
 # randomly permutate @array in place
-sub fisher_yates_shuffle
-{
-    my $array = shift;
-    my $i = @$array;
-    while ( --$i )
-    {
-        my $j = int rand( $i+1 );
-        @$array[$i,$j] = @$array[$j,$i];
+sub fisher_yates_shuffle {
+  my $array = shift;
+  my $i = @$array;
+  while ( --$i ) {
+    my $j = int rand( $i+1 );
+    @$array[$i,$j] = @$array[$j,$i];
+  }
+}
+
+sub initialize_stack {
+  my $array = shift;
+
+  foreach my $suit (@suites) {
+    foreach my $value (@values) {
+      push(@$array, "$value$suit");
     }
+  }
 }

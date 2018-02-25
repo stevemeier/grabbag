46a390576b475a0502c35bfab030c6966c15c756
stevemeier <email@steve-meier.de>
2018-02-25 20:54:08 +0100

    Now with Jacks :-D

--- playing_cards.pl
+++ playing_cards.pl
@@ -2,21 +2,28 @@
 
 use strict;
 use warnings;
+use Data::Dumper;
+use List::Util qw(shuffle);
 
 my @suites = ("♤ ","♧ ","♡ ","♢ ");
-my @values = (2 .. 9, "T", "Q", "K", "A");
+my @values = (2 .. 9,"T","J","Q","K","A");
+
+my %pocket;
 
 my @stack;
 initialize_stack(\@stack);
 
-foreach (1 .. 2000000) {
-  fisher_yates_shuffle(\@stack);
+foreach (1 .. 10000000) {
+# fisher_yates_shuffle(\@stack);
+  @stack = shuffle @stack;
 
-  if (($stack[0] =~ /A/) && ($stack[1] =~ /A/)) {
-    print "Aces!\n";
-  }
+  if (($stack[0] =~ /^A/) && ($stack[1] =~ /^A/)) { $pocket{'aces'}++; }
+  if (($stack[0] =~ /^K/) && ($stack[1] =~ /^K/)) { $pocket{'kings'}++; }
+  if (($stack[0] =~ /^Q/) && ($stack[1] =~ /^Q/)) { $pocket{'queens'}++; }
+  if (($stack[0] =~ /^J/) && ($stack[1] =~ /^J/)) { $pocket{'jacks'}++; }
 }
 
+print Dumper %pocket;
 exit;
 
 # from http://www.perlmonks.org/?node_id=1869

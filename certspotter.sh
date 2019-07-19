5420df4642a553d3dbd08ea6fdac21fa9b1e146b
stevemeier <email@steve-meier.de>
2019-07-19 13:02:51 +0200

    Added cert-chain-resolver to certspotter.sh

--- certspotter.sh
+++ certspotter.sh
@@ -7,5 +7,6 @@ export SSLMATE_APIKEY=get-your-own-jey
 ./certspotter --cert /usr/syno/etc/certificate/system/default/cert.pem
 
 if [ $? -eq 0 ]; then
+  ./cert-chain-resolver -o /usr/syno/etc/certificate/system/default/fullchain.pem
   /usr/syno/sbin/synoservicectl --restart nginx
 fi

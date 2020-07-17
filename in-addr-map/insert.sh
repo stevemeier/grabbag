8b55ff2b287df9b5544b8be83b14e10cb81beed9
stevemeier <email@steve-meier.de>
2020-07-17 21:49:45 +0200

    Re-add nextip Index which impacts performance big time

--- in-addr-map/insert.sh
+++ in-addr-map/insert.sh
@@ -6,6 +6,7 @@ cat <<EOF
 CREATE TABLE t1 (ipint integer primary key autoincrement, o1 int, o2 int, o3 int, o4 int, rcode int, ptr text, lastupd integer default 0);
 CREATE INDEX ptr_index on t1(ptr);
 CREATE INDEX lastupd_index on t1(lastupd);
+CREATE INDEX nextip on t1(o1, o2, o3, o4, lastupd);
 EOF
 
 A=${1:-1}

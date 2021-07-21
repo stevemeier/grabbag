09ebb9b81ce2f0617d3efb44f9d0914adcff562a
stevemeier <email@steve-meier.de>
2021-07-21 11:28:53 +0200

    First working version, some todo's open

--- dynag-dns/main.go
+++ dynag-dns/main.go
@@ -1,17 +1,26 @@
 package main
 
 import "bytes"
-import "fmt"
 import "log"
 import "os/exec"
+import "strconv"
+import "strings"
+import "time"
+import "github.com/gofrs/uuid"
 import "github.com/miekg/dns"
 import "github.com/whiteShtef/clockwork"
+import config "github.com/olebedev/config"
 
 type checkResult struct {
-	qname	string
-	qtype	string
-	data	string
-	success	bool
+	Uuid	string
+	Success	bool
+}
+
+type DynRR struct {
+	Data		dns.RR
+	Enabled		bool
+	Uuid		string
+	LastChange	time.Time
 }
 
 var resultChan chan checkResult
@@ -19,30 +28,69 @@ var DNSrequests chan *dns.Msg
 var DNSresponses chan *dns.Msg
 
 func main() {
+	// Initialize channels
         resultChan = make(chan checkResult, 20)
 	DNSrequests = make(chan *dns.Msg, 20)
 	DNSresponses = make(chan *dns.Msg, 20)
 
-	go processor()
+	// Initialize DNS data structure
+	dnsdata := make(map[string][]*DynRR)
+
+	// Load config file
+	cfg, conferr := LoadConfig("./config.json")
+	if conferr != nil {
+		log.Fatal(conferr)
+	}
+
+	// Set up the scheduler
 	sched := clockwork.NewScheduler()
-//	sched.Schedule().Every(1).Minutes().Do( func() { run_check("/usr/bin/false") } )
-	sched.Schedule().Every(15).Seconds().Do( func() { run_check("/bin/ls", []string{"/Users/smeier/tmp/foo"}) } )
+
+	// Iterate over names in configuration
+	for i := 0; i < len(cfg.UList("names")); i++ {
+		uuid := GenerateUUID()
+		nameconf, _ := cfg.Get("names." + strconv.Itoa(i))
+		newrr, _ := dns.NewRR(nameconf.UString("name") + " " + nameconf.UString("rr"))
+		header := newrr.Header()
+
+		// Add name to the DNS data structure
+		mapkey := header.Name + "/" + dns.TypeToString[header.Rrtype]
+		dnsdata[mapkey] = append(dnsdata[mapkey], &DynRR{Data: newrr, Enabled: false, Uuid: uuid})
+
+		// Schedule checks for this entry
+		sched.Schedule().Every(nameconf.UInt("interval")).Seconds().Do( func() { run_check(nameconf.UString("command"), uuid) })
+	}
+
+	// Start the processing of check results and DNS queries
+	go processor(dnsdata)
+
+	// Run the previously configured scheduler
 	go sched.Run()
 
+	// Register the DNS handler
 	dns.HandleFunc(".", handleDnsRequest)
 
-	// start server
-	port := 5300
-	server := &dns.Server{Addr: "0.0.0.0:5300", Net: "udp"}
-	log.Printf("Starting at %d\n", port)
-	err := server.ListenAndServe()
-	log.Println(err)
+	// Set up the server object and listen for requests
+	udpserver := &dns.Server{Addr: "0.0.0.0:5300", Net: "udp"}
+	tcpserver := &dns.Server{Addr: "0.0.0.0:5300", Net: "tcp"}
+	go func(){ _ = udpserver.ListenAndServe() }()
+	go func(){ _ = tcpserver.ListenAndServe() }()
+
+	// Don't exit
+	select {}
 }
 
-func run_check (command string, args []string) {
-	_, rcode, err := sysexec(command, args, nil)
+func run_check (command string, uuid string) {
+	// Split the command into it's first bit (the binary) and the rest (the arguments)
+	comsplit := strings.Split(command, ` `)
+
+	// Call the sysexec function to run the check (nil == no input to the called binary)
+	_, rcode, err := sysexec(comsplit[0], comsplit[1:], nil)
+
 	if err == nil {
-		resultChan <- checkResult{"foo.example.org.", "A", "1.2.3.4", rcode == 0}
+		// Put the return code of the check into the results channel
+		resultChan <- checkResult{Uuid: uuid, Success: rcode == 0}
+	} else {
+		log.Println("%s -> %s\n", comsplit[0], err.Error())
 	}
 	return
 }
@@ -63,107 +111,106 @@ func sysexec (command string, args []string, input []byte) ([]byte, int, error) 
         return output.Bytes(), exitcode, err
 }
 
-func processor () {
-	dnsdata := make(map[string]map[string][]string)
-
+func processor (dnsdata map[string][]*DynRR) {
 	go func() {
-	for latest := range resultChan {
-		if latest.success {
-			log.Printf("Adding: %s %s %s\n", latest.qname, latest.qtype, latest.data)
-			// add record as test was successful
-			if dnsdata[latest.qname] == nil {
-				dnsdata[latest.qname] = make(map[string][]string)
-			}
-			dnsdata[latest.qname][latest.qtype] = append(dnsdata[latest.qname][latest.qtype], latest.data)
-			dnsdata[latest.qname][latest.qtype] = makeUnique(dnsdata[latest.qname][latest.qtype])
-		}
-		if !latest.success {
-			log.Printf("Deleting: %s %s %s\n", latest.qname, latest.qtype, latest.data)
-			// delete record as test has failed
-			if _, okn := dnsdata[latest.qname]; okn {
-				if _, okt := dnsdata[latest.qname][latest.qtype]; okt {
-					dnsdata[latest.qname][latest.qtype] = removeEntry(dnsdata[latest.qname][latest.qtype], latest.data)
+	// Process check results
+	for check := range resultChan {
+		// Find the record this check result applies to
+		// Inefficient, but this is a proof of concept
+		for mapkey, _ := range dnsdata {
+			for _, record := range dnsdata[mapkey] {
+				if record.Uuid == check.Uuid && record.Enabled != check.Success {
+					// Change the boolean status, log it, and set a timestamp (for debugging)
+					log.Printf("Status change for %s to %s\n",
+					record.Data.String(),
+					UpBool(check.Success))
+
+					record.Enabled = check.Success
+					record.LastChange = time.Now()
 				}
 			}
 		}
 	}
-}()
+	}()
 
 	go func() {
-	for dnsq := range DNSrequests {
+	// Process DNS queries
+	for query := range DNSrequests {
+		// Setup an answer object
 		answer := new(dns.Msg)
-		answer.SetReply(dnsq)
 		answer.Compress = false
 
-		switch dnsq.Question[0].Qtype {
-		case dns.TypeA:
-			_, okn := dnsdata[dnsq.Question[0].Name]
-			if okn {
-				_, okt := dnsdata[dnsq.Question[0].Name]["A"]
-				if okt {
-					for _, ip4 := range dnsdata[dnsq.Question[0].Name]["A"] {
-						rr, err := dns.NewRR(fmt.Sprintf("%s 60 IN A %s", dnsq.Question[0].Name, ip4))
-						if err == nil {
-							log.Println(rr)
-							answer.Answer = append(answer.Answer, rr)
-						} else {
-							log.Println(err)
-						}
-					}
-				}
+		// Handle non INET class (respond with NOTIMP)
+		if query.Question[0].Qclass != dns.ClassINET {
+			answer.SetRcode(query, dns.StringToRcode["NOTIMP"])
+			DNSresponses <- answer
+			continue
+		}
+
+		// Find matching records in dnsdata map (e.g. mapkey = `www.example.org./A`)
+		mapkey := query.Question[0].Name + "/" + dns.TypeToString[query.Question[0].Qtype]
+
+		// If this record is not in the map, we return NXDOMAIN
+		if len(dnsdata[mapkey]) == 0 {
+			answer.SetRcode(query, dns.StringToRcode["NXDOMAIN"])
+			DNSresponses <- answer
+			continue
+		}
+
+		// We need to check if at least one record is `Enabled`
+		// If there is none, return SERVFAIL, to prevent caching
+		enabled := 0
+		for _, record := range dnsdata[mapkey] {
+			if record.Enabled { enabled++ }
+		}
+
+		if enabled == 0 {
+			answer.SetRcode(query, dns.StringToRcode["SERVFAIL"])
+			DNSresponses <- answer
+			continue
+		}
+
+		// At this point, data should be available, so we construct a proper answer
+		answer.SetReply(query)
+		for _, record := range dnsdata[mapkey] {
+			if record.Enabled {
+				answer.Answer = append(answer.Answer, record.Data)
 			}
-
-		case dns.TypeAAAA:
-		default:
-			answer.SetRcode(dnsq, 4)	// NOTIMP
 		}
 
-		if len(answer.Answer) == 0 {
-			answer.SetRcode(dnsq, 2)	// SERVFAIL
-		}
-
+		// Send our compiled answer
 		DNSresponses <- answer
 	}
-}()
-}
-
-// https://cyruslab.net/2019/11/07/goremove-duplicate-elements-in-array-slice/
-func makeUnique(names []string) []string {
-    // if the name occurs the flag changes to true.
-    // hence all name that has already occurred will be true.
-    flag := make(map[string]bool)
-    var uniqueNames []string
-    for _, name := range names {
-        if flag[name] == false {
-            flag[name] = true
-            uniqueNames = append(uniqueNames, name)
-        }
-    }
-    // unique names collected
-    return uniqueNames
-}
-
-func removeEntry(names []string, remove string) []string {
-	var result []string
-	for _, name := range names {
-		if name == remove { continue }
-		result = append(result, name)
-	}
-
-	return result
+	}()
 }
 
 func handleDnsRequest (w dns.ResponseWriter, r *dns.Msg) {
-	log.Printf("DNS request received: %s/%s\n", r.Question[0].Name, QtypeHumanReadable(r.Question[0].Qtype))
+	// Log
+	log.Printf("DNS request received: %s/%s from %s\n", r.Question[0].Name, dns.TypeToString[r.Question[0].Qtype], w.RemoteAddr())
+
+	// Put the request into the DNSrequests channel
 	DNSrequests <- r
-	w.WriteMsg(<-DNSresponses)
+
+	// Write the latest response from the DNSresponses channel to the network
+	werr := w.WriteMsg(<-DNSresponses)
+	if werr != nil {
+		log.Println(werr)
+	}
+
 	return
 }
 
-func QtypeHumanReadable (qtype uint16) (string) {
-	switch qtype {
-	case 1: return `A`
-	case 28: return `AAAA`
-	}
-	return ``
+func GenerateUUID () string {
+	// Generates a temporary UUID (only used at runtime, not stored or configurable)
+	result, _ := uuid.NewV4()
+	return result.String()
+}
+
+func LoadConfig (path string) (*config.Config, error) {
+  cfg, err := config.ParseJsonFile(path)
+  return cfg, err
+}
+
+func UpBool (in bool) string {
+  return strings.ToUpper(strconv.FormatBool(in))
 }

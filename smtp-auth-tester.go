40150d2043df00fc910c6f077646fc2e1d2f9e1c
stevemeier <email@steve-meier.de>
2022-12-18 21:05:52 +0100

    Add STARTTLS support and use net/smtp module

--- smtp-auth-tester.go
+++ smtp-auth-tester.go
@@ -2,17 +2,15 @@ package main
 
 import "crypto/hmac"
 import "crypto/md5"
+import "crypto/tls"
 import b64 "encoding/base64"
 import "fmt"
 import "io"
 import "log"
-import "net"
+import "net/smtp"
 import "os"
-import "regexp"
 import "strings"
-import "time"
 
-import "github.com/davecgh/go-spew/spew"
 import "github.com/DavidGamba/go-getoptions"
 
 func main() {
@@ -21,10 +19,12 @@ func main() {
 	var password string
 	var server string
 	var port string
+	var insecure bool
 	opt.StringVar(&server, "server", "", opt.Alias("s"), opt.Required())
 	opt.StringVar(&port, "port", "25", opt.Alias("n"))
 	opt.StringVar(&username, "username", "", opt.Alias("u"), opt.Required())
 	opt.StringVar(&password, "password", "", opt.Alias("p"), opt.Required())
+	opt.BoolVar(&insecure, "insecure", false)
 	remaining, opterr := opt.Parse(os.Args[1:])
 	if opterr != nil {
 		log.Fatal(opterr)
@@ -33,81 +33,81 @@ func main() {
 		log.Printf("Unhandled parameters: %v\n", remaining)
 	}
 
-	var authmethods []string
-	serverok := regexp.MustCompile("^2")
+	var authmethods string
+	tlsconfig := &tls.Config{
+		InsecureSkipVerify: insecure,
+		ServerName: server,
+	}
 
 	log.Printf("Connecting to %s:%s\n", server, port)
-	conn, connerr := net.Dial("tcp", server+":"+port)
+	conn, connerr := smtp.Dial(server+":"+port)
 
 	if connerr != nil {
 		log.Fatal(connerr)
 	}
 
-	authmethods = get_auth_methods(conn)
-	spew.Dump(authmethods)
-	fmt.Fprintf(conn, "QUIT\r\n")
+	_, authmethods = conn.Extension("AUTH")
+	log.Printf("Server supports: %s\n", authmethods)
+	conn.Quit()
 	conn.Close()
 
-	for _, method := range authmethods {
+	for _, method := range strings.Split(authmethods, " ") {
 		// Connect and read banner
-		conn, connerr := net.Dial("tcp", server+":"+port)
+		conn, connerr := smtp.Dial(server+":"+port)
 		if connerr != nil {
 			log.Fatalf("Could not connect: %s\n", connerr)
 		}
-		read_server_message(conn)
 
 		fmt.Printf("Next method: %s\n", method)
 		if method == "LOGIN" {
-			fmt.Fprintf(conn, "AUTH LOGIN\r\n")
-			read_server_message(conn)
-			fmt.Fprintf(conn, "%s\r\n", base64(username))
-			read_server_message(conn)
-			fmt.Fprintf(conn, "%s\r\n", base64(password))
-			response := read_server_message(conn)
-			if serverok.Match(response) {
+			conn.Text.Writer.PrintfLine("AUTH LOGIN")
+			read_server_response(conn)
+			conn.Text.Writer.PrintfLine("%s", base64(username))
+			read_server_response(conn)
+			conn.Text.Writer.PrintfLine("%s", base64(password))
+			code, response := read_server_response(conn)
+			if code >= 200 && code < 300 {
 				fmt.Println("AUTH LOGIN: SUCCESS\n")
 			} else {
 				fmt.Printf("AUTH LOGIN: FAILED -> %s\n", response)
 			}
-			fmt.Fprintf(conn, "QUIT\r\n")
 		}
+
 		if method == "PLAIN" {
-			fmt.Fprintf(conn, "AUTH PLAIN\r\n")
-			read_server_message(conn)
-			fmt.Fprintf(conn, "%s\r\n", base64(username+"\000"+username+"\000"+password))
-			response := read_server_message(conn)
-			if serverok.Match(response) {
+			tlserr := conn.StartTLS(tlsconfig)
+			if tlserr != nil {
+				log.Println(tlserr)
+			} else {
+				log.Println("STARTTLS OK")
+			}
+			plainauth := smtp.PlainAuth(username, username, password, server)
+			perr := conn.Auth(plainauth)
+			if perr != nil {
+				fmt.Printf("AUTH PLAIN: FAILED -> %s\n", perr.Error())
+			} else {
 				fmt.Println("AUTH PLAIN: SUCCESS\n")
-			} else {
-				fmt.Printf("AUTH PLAIN: FAILED -> %s\n", response)
 			}
-			fmt.Fprintf(conn, "QUIT\r\n")
 		}
+
 		if method == "CRAM-MD5" {
-			fmt.Fprintf(conn, "AUTH CRAM-MD5\r\n")
-			challenge := unbase64(string(read_server_message(conn)[4:]))
-			fmt.Fprintf(conn, "%s\r\n", base64(username+" "+hmac_md5_hex(challenge,password)))
-			response := read_server_message(conn)
-			if serverok.Match(response) {
-				fmt.Println("AUTH CRAM-MD5: SUCCESS\n")
+			cramauth := smtp.CRAMMD5Auth(username, password)
+			cerr := conn.Auth(cramauth)
+			if cerr != nil {
+				fmt.Printf("AUTH CRAM-MD5: FAILED -> %s\n", cerr.Error())
 			} else {
-				fmt.Printf("AUTH CRAM-MD5: FAILED -> %s\n", response)
+				fmt.Println("AUTH CRAM-MD5: SUCCESS\n")
 			}
-			fmt.Fprintf(conn, "QUIT\r\n")
 		}
-		time.Sleep(1 * time.Second)
+
+		conn.Quit()
 		conn.Close()
 	}
+
 }
 
-func read_server_message (c net.Conn) ([]byte) {
-	buffer := make([]byte, 1024)
-	_, err := c.Read(buffer)
-	if err != nil {
-		return []byte{}
-	}
-
-	return buffer
+func read_server_response (c *smtp.Client) (int, string) {
+	code, message, _ := c.Text.ReadCodeLine(-1)
+	return code, message
 }
 
 func error_out (m []byte) () {
@@ -121,24 +121,6 @@ func base64 (s string) (string) {
 func unbase64 (s string) (string) {
 	result, _ := b64.StdEncoding.DecodeString(s)
 	return string(result)
-}
-
-func get_auth_methods (c net.Conn) ([]string) {
-	var result []string
-
-	serverok := regexp.MustCompile("^2")
-	authre := regexp.MustCompile("(AUTH.)(.*)\\r")
-
-	read_server_message(c)
-	fmt.Fprintf(c, "EHLO example.com\r\n")
-	response := read_server_message(c)
-	if serverok.Match(response) {
-		authdata := authre.FindStringSubmatch(string(response))
-		if len(authdata) == 0 { error_out([]byte("Authentication not supported")) }
-		result = strings.Split(authdata[2], " ")
-	}
-
-	return result
 }
 
 func hmac_md5_hex (challenge string, password string) string {
